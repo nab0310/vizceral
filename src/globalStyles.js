186aba6c09c2c74d655ead2b5ed1397d98360f10
Nick Behrens <behrensn@iastate.edu>
2017-06-16 09:17:23 -0500

    Update globalStyles.js

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -37,22 +37,22 @@ class GlobalStyles {
       colorText: 'rgb(214, 214, 214)',
       colorTextDisabled: 'rgb(129, 129, 129)',
       colorTraffic: {
-        normal: 'rgb(186, 213, 237)',
-        normalDonut: 'rgb(91, 91, 91)',
+        normal: 'rgb(13, 148, 210 )',
+        normalDonut: 'rgb(123, 193, 67)',
         warning: 'rgb(268, 185, 73)',
         danger: 'rgb(184, 36, 36)'
       },
-      colorNormalDimmed: 'rgb(101, 117, 128)',
+      colorNormalDimmed: 'rgb(123, 193, 67)',
       colorBackgroundDark: 'rgb(35, 35, 35)',
       colorLabelBorder: 'rgb(16, 17, 18)',
       colorLabelText: 'rgb(0, 0, 0)',
-      colorDonutInternalColor: 'rgb(35, 35, 35)',
+      colorDonutInternalColor: 'rgb(255, 255, 255)',
       colorDonutInternalColorHighlighted: 'rgb(255, 255, 255)',
       colorConnectionLine: 'rgb(91, 91, 91)',
-      colorPageBackground: 'rgb(45, 45, 45)',
+      colorPageBackground: 'rgb(255, 255, 255)',
       colorPageBackgroundTransparent: 'rgba(45, 45, 45, 0)',
       colorBorderLines: 'rgb(137, 137, 137)',
-      colorArcBackground: 'rgb(60, 60, 60)'
+      colorArcBackground: 'rgb(255, 255, 255)'
     };
 
     this.updateComputedStyles();
